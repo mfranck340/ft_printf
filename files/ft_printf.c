f64d8cbc7acc8e2aff884c7a6bf5f43d2a5c53ae
mfranck340 <franck.yaguargos@edu.uah.es>
2024-10-09 12:22:11 +0200

    add lines

--- files/ft_printf.c
+++ files/ft_printf.c
@@ -1,6 +1,57 @@
 
 #include "ft_printf.h"
 
+int	print_char(char c)
+{
+	ft_putchar(c);
+	return (1);
+}
+
+int	print_string(char *str)
+{
+	int		n_ret;
+
+	n_ret = 0;
+	if (!str)
+	{
+		ft_putstr("(null)");
+		return (6);
+	}
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+		n_ret++;
+	}
+	return (n_ret);
+}
+
+int	print_format(char const *format, va_list args)
+{
+	if (*format == 'c')
+		return (print_char(va_arg(args, int)));
+	else if (*format == 's')
+		return (print_string(va_arg(args, char *)));
+	else if (*format == 'd' || *format == 'i')
+		return (print_int(va_arg(args, int)));
+	else if (*format == 'u')
+		return (print_unsigned(va_arg(args, unsigned int)));
+	else if (*format == 'x')
+		return (print_hex(va_arg(args, unsigned int), 0));
+	else if (*format == 'X')
+		return (print_hex(va_arg(args, unsigned int), 1));
+	else if (*format == 'p')
+		return (print_pointer(va_arg(args, void *)));
+	else if (*format == '%')
+		return (print_char('%'));
+	else
+	{
+		ft_putchar('%');
+		ft_putchar(*format);
+		return (2);
+	}
+}
+
 int	print_message(char const *format, va_list args)
 {
 	int		n_ret;
@@ -8,10 +59,10 @@ int	print_message(char const *format, va_list args)
 	n_ret = 0;
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%' && *(format + 1) != '\0')
 		{
 			format++;
-			n_ret += print_format(&format, args);
+			n_ret += print_format(format, args);
 		}
 		else
 		{
@@ -28,6 +79,8 @@ int	ft_printf(char const *format, ...)
 	va_list	args;
 	int		n_ret;
 
+	if (!format)
+		return (-1);
 	va_start(args, format);
 	n_ret = print_message(format, args);
 	va_end(args);
---
+++ files/ft_printf.h
@@ -0,0 +1,9 @@
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+
+int	ft_printf(char const *format, ...);
+
+#endif
