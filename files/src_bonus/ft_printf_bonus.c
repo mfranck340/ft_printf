f98efc7aab08caf3a4fd7c0ee5ffdf6f40d4edce
mfranck340 <franck.yaguargos@edu.uah.es>
2024-10-14 00:21:32 +0200

    bonus without norme

--- files/src_bonus/ft_printf_bonus.c
+++ files/src_bonus/ft_printf_bonus.c
@@ -12,6 +12,7 @@
 
 #include "../include/ft_printf_bonus.h"
 #include <stdio.h>
+#include <unistd.h>
 
 int	print_format(char const *format, va_list args)
 {
@@ -39,16 +40,11 @@ int	print_format(char const *format, va_list args)
 	}
 }
 
-int	print_n_string(char *str, int n)
+int	print_n_string(char *str, unsigned int n)
 {
 	int		n_ret;
 
 	n_ret = 0;
-	if (!str && n >= 6)
-	{
-		ft_putstr_fd("(null)", 1);
-		return (6);
-	}
 	while (str && n--)
 	{
 		ft_putchar_fd(*str, 1);
@@ -58,11 +54,41 @@ int	print_n_string(char *str, int n)
 	return (n_ret);
 }
 
+int	ft_numlen(unsigned int n, unsigned int base)
+{
+	if (n < base)
+		return (1);
+	else
+	{
+		return (1 + ft_numlen(n / base, base));
+	}
+}
+
+int	ft_numpointer(void *p)
+{
+	unsigned long	n;
+	int n_aux;
+
+	if (!p)
+		return (5);
+	n = (unsigned long)p;
+	n_aux = 0;
+	while (n)
+	{
+		n = n / 16;
+		n_aux++;
+	}
+	return(n_aux);
+}
+
 int	print_format_flags(va_list args, t_flags flags)
 {
 	char *str;
 	int n_aux;
 	int	number;
+	unsigned int hex_number;
+	void	*pointer;
+
 
 	int	n_str;
 
@@ -96,15 +122,33 @@ int	print_format_flags(va_list args, t_flags flags)
 		n_aux = n_str;
 		if (flags.minus)
 		{
-			print_n_string(str, n_aux);
+			if (!str && (flags.dot == 0 || flags.precision >= 6))
+			{
+				print_n_string("(null)", 6);
+				n_str += 6;
+				n_aux = 6;
+
+			}
+			else
+				print_n_string(str, n_aux);
 			while (flags.width-- > n_aux)
 				n_str += print_char(' ');
 		}
 		else
 		{
-			while (flags.width-- > n_aux)
-				n_str += print_char(' ');
-			print_n_string(str, n_aux);
+			if (!str && (flags.dot == 0 || flags.precision >= 6))
+			{
+				while (flags.width-- > 6)
+					n_str += print_char(' ');
+				print_n_string("(null)", 6);
+				n_str += 6;
+			}
+			else
+			{
+				while (flags.width-- > n_aux)
+					n_str += print_char(' ');
+				print_n_string(str, n_aux);
+			}
 		}
 	}
 	else if (flags.type == 'd' || flags.type == 'i')
@@ -139,7 +183,10 @@ int	print_format_flags(va_list args, t_flags flags)
 		else if (flags.minus)
 		{
 			if (number < 0)
+			{
 				print_char('-');
+				n_aux--;
+			}
 			else if (number >= 0 && flags.plus)
 			{
 				print_char('+');
@@ -169,14 +216,38 @@ int	print_format_flags(va_list args, t_flags flags)
 		{
 			if (number < 0 || flags.plus || flags.space)
 			{
-				if (flags.width > flags.precision + 1)
-					while (flags.width-- > flags.precision + 1)
-						n_str += print_char(' ');
+				if (flags.dot)
+				{
+					if (flags.width > flags.precision + 1)
+						while (flags.width-- > flags.precision + 1)
+							n_str += print_char(' ');
+				}
+				else if (number < 0)
+				{
+					if (flags.width > n_aux)
+						while (flags.width-- > n_aux)
+							n_str += print_char(' ');
+				}
+				else
+				{
+					if (flags.width > n_aux + 1)
+						while (flags.width-- > n_aux + 1)
+							n_str += print_char(' ');
+				}
 			}
 			else
-				if (flags.width > flags.precision)
-					while (flags.width-- > flags.precision)
-						n_str += print_char(' ');
+				if (flags.dot)
+				{
+					if (flags.width > flags.precision)
+						while (flags.width-- > flags.precision)
+							n_str += print_char(' ');
+				}
+				else
+				{
+					if (flags.width > n_aux)
+						while (flags.width-- > n_aux)
+							n_str += print_char(' ');
+				}
 			if (number < 0)
 			{
 				print_char('-');
@@ -207,7 +278,216 @@ int	print_format_flags(va_list args, t_flags flags)
 		}
 		free(str);
 	}
-
+	else if (flags.type == 'u')
+	{
+		hex_number = va_arg(args, unsigned int);
+		n_str = ft_numlen(hex_number, 10);
+		n_aux = n_str;
+		if (flags.zero && flags.dot == 0 && flags.minus == 0)
+		{
+			while (flags.width-- > n_aux)
+				n_str += print_char('0');
+			print_unsigned(hex_number);
+		}
+		else if (flags.minus)
+		{
+			if (flags.precision > n_aux)
+			{
+				while (flags.precision-- > n_aux)
+					n_str += print_char('0');
+			}
+			print_unsigned(hex_number);
+			n_aux = n_str;
+			while (flags.width-- > n_aux)
+				n_str += print_char(' ');
+		}
+		else
+		{
+			if (flags.dot && flags.precision > n_aux)
+			{
+				if (flags.width > flags.precision)
+					while (flags.width-- > flags.precision)
+						n_str += print_char(' ');
+				if (flags.precision > n_aux)
+					while (flags.precision-- > n_aux)
+						n_str += print_char('0');
+			}
+			else
+			{
+				if (flags.width > n_aux)
+					while (flags.width-- > n_aux)
+						n_str += print_char(' ');
+			}
+			print_unsigned(hex_number);
+		}
+	}
+	else if (flags.type == 'x' || flags.type == 'X')
+	{
+		hex_number = va_arg(args, unsigned int);
+		n_str = ft_numlen(hex_number, 16);
+		n_aux = n_str;
+		if (flags.zero && flags.dot == 0 && flags.minus == 0)
+		{
+			if (flags.hashtag && hex_number != 0)
+			{
+				print_char('0');
+				print_char(flags.type);
+				n_str += 2;
+				n_aux += 2;
+			}
+			while (flags.width-- > n_aux)
+				n_str += print_char('0');
+			if (flags.type == 'x')
+				print_hex(hex_number, HEX_LOWER);
+			else
+				print_hex(hex_number, HEX_UPPER);
+		}
+		else if (flags.minus)
+		{
+			if (flags.hashtag && hex_number != 0)
+			{
+				print_char('0');
+				print_char(flags.type);
+				n_str += 2;
+			}
+			if (flags.precision > n_aux)
+			{
+				while (flags.precision-- > n_aux)
+					n_str += print_char('0');
+			}
+			if (flags.type == 'x')
+				print_hex(hex_number, HEX_LOWER);
+			else
+				print_hex(hex_number, HEX_UPPER);
+			n_aux = n_str;
+			while (flags.width-- > n_aux)
+				n_str += print_char(' ');
+		}
+		else
+		{
+			if (flags.hashtag && hex_number != 0)
+			{
+				if (flags.dot)
+				{
+					if (flags.width > flags.precision + 2)
+						while (flags.width-- > flags.precision + 2)
+							n_str += print_char(' ');
+				}
+				else
+				{
+					if (flags.width > n_aux + 2)
+						while (flags.width-- > n_aux + 2)
+							n_str += print_char(' ');
+				}
+				print_char('0');
+				print_char(flags.type);
+				n_str += 2;
+			}
+			else
+			{
+				if (flags.dot)
+				{
+					if (flags.width > flags.precision)
+						while (flags.width-- > flags.precision)
+							n_str += print_char(' ');
+				}
+				else
+				{
+					if (flags.width > n_aux)
+						while (flags.width-- > n_aux)
+							n_str += print_char(' ');
+				}
+			}
+			if (flags.precision > n_aux)
+				while (flags.precision-- > n_aux)
+					n_str += print_char('0');
+			if (flags.type == 'x')
+				print_hex(hex_number, HEX_LOWER);
+			else
+				print_hex(hex_number, HEX_UPPER);
+		}
+	}
+	else
+	{
+		pointer = va_arg(args, void *);
+		n_str = ft_numpointer(pointer);
+		n_aux = n_str;
+		if (flags.zero && flags.dot == 0 && flags.minus == 0)
+		{
+			if (!pointer)
+			{
+				while (flags.width-- > n_aux)
+					n_str += print_char(' ');
+				ft_putstr_fd("(nil)", 1);
+			}
+			else
+			{
+				ft_putchar_fd('0', 1);
+				ft_putchar_fd('x', 1);
+				n_str += 2;
+				while (flags.width-- > n_aux)
+					n_str += print_char('0');
+				print_hex((unsigned long) pointer, HEX_LOWER);
+			}
+		}
+		else if (flags.minus)
+		{
+			if (!pointer)
+			{
+				ft_putstr_fd("(nil)", 1);
+				while (flags.width-- > 5)
+					n_str += print_char(' ');
+			}
+			else
+			{
+				ft_putchar_fd('0', 1);
+				ft_putchar_fd('x', 1);
+				n_str += 2;
+				if (flags.precision > n_aux)
+				{
+					while (flags.precision-- > n_aux)
+						n_str += print_char('0');
+				}
+				n_aux = n_str;
+				print_hex((unsigned long) pointer, HEX_LOWER);
+				while (flags.width-- > n_aux)
+					n_str += print_char(' ');
+			}
+		}
+		else
+		{
+			if (!pointer)
+			{
+				while (flags.width-- > 5)
+					n_str += print_char(' ');
+				ft_putstr_fd("(nil)", 1);
+			}
+			else
+			{
+				if (flags.dot && flags.precision > n_aux)
+				{
+					if (flags.width > flags.precision + 2)
+						while (flags.width-- > flags.precision + 2)
+							n_str += print_char(' ');
+				}
+				else
+				{
+					if (flags.width > n_aux + 2)
+						while (flags.width-- > n_aux + 2)
+							n_str += print_char(' ');
+				}
+				ft_putchar_fd('0', 1);
+				ft_putchar_fd('x', 1);
+				n_str += 2;
+				if (flags.precision > n_aux)
+				{
+					while (flags.precision-- > n_aux)
+						n_str += print_char('0');
+				}
+				print_hex((unsigned long) pointer, HEX_LOWER);
+			}
+		}
+	}
 	return (n_str);
 }
 
--- main.c
+++ main.c
@@ -3,37 +3,195 @@
 
 int    main(void)
 {
+    /*
     int a;
 	char *str = "cositas";
-    /*
+    
     a = ft_printf("Hello, %s! \n %u / %d / %d // %p // %x - %X\n", str, 3147483647, -42, -2147483648, str, 65436, 56456);
     ft_printf("ft_printf: %d\n", a);
     a = printf("Hello, %s! \n %u / %d / %d // %p // %x - %X\n", str, 3147483647, -42, -2147483648, str, 65436, 56456);
-    printf("printf: %d\n\n\n", a);*/
+    printf("printf: %d\n\n\n", a);
 
-    a = ft_printf("%7.7s%7.7s", "hello", "world");
+    a = ft_printf("%.c", 'a');
     ft_printf("ft_printf: %d\n", a);
-    a = ft_printf(" %+d ", 0);
+    a = printf("%.c", 'a');
+    printf("printf: %d\n\n\n", a);
+
+    a = ft_printf("%-5%");
     ft_printf("ft_printf: %d\n", a);
-    a = printf(" %+d ", 0);
+    a = printf("%-5%");
     printf("printf: %d\n\n\n", a);
     printf("\n------------------------------------\n");
-    a = printf("%+u\n", 3432);
-    a = printf("%+d\n", 3432);
-    printf("%020p\n", str);
-    printf("%-20p\n", str);
-    printf("%20p\n", str);
+    a = ft_printf("%-15%# +d \n", 'c');
+    a = ft_printf("% 7.5d# +d \n", 512);
+    a = ft_printf("%- 10d# +d \n", 512);
+    a = ft_printf("%0 7d# +d \n", 512);
+    a = ft_printf("%015s# +d \n", "holahola");
+    ft_printf("printf: %d\n\n\n", a);
+
     a = printf("%-15%# +d \n", 'c');
     a = printf("% 7.5d# +d \n", 512);
     a = printf("%- 10d# +d \n", 512);
     a = printf("%0 7d# +d \n", 512);
     a = printf("%015s# +d \n", "holahola");
     printf("printf: %d\n\n\n", a);
+    printf("\n------------------------------------\n");*/
 
-    a = ft_printf("|%-0# +10.8%|\n", 255);
-    ft_printf("ft_printf: %d\n", a);
-    a = printf("|%-0# +.8%|\n", 255);
-    printf("printf: %d\n\n\n", a);
+    /*
+    int num = 42;
+    unsigned int unum = 3000;
+    float fnum = 3.14159;
+    char *str = "Hola";
+    void *ptr = &num;
+
+    // 1. Flags con %d (entero con signo)
+    printf("1. |%+08d|\n", num);     // Signo siempre, relleno con ceros, ancho mínimo de 8
+    printf("2. |%-8d|\n", num);      // Alineación a la izquierda, ancho mínimo de 8
+    printf("3. |% 8d|\n", num);      // Espacio para positivo, ancho mínimo de 8
+    ft_printf("1. |%+08d|\n", num);     // Signo siempre, relleno con ceros, ancho mínimo de 8
+    ft_printf("2. |%-8d|\n", num);      // Alineación a la izquierda, ancho mínimo de 8
+    ft_printf("3. |% 8d|\n", num);
+    
+    printf("\n------------------------------------\n");
+    // 2. Flags con %u (entero sin signo)
+    printf("4. |%08u|\n", unum);     // Relleno con ceros, ancho mínimo de 8
+    printf("5. |%-8u|\n", unum);     // Alineación a la izquierda, ancho mínimo de 8
+
+    ft_printf("4. |%08u|\n", unum);     // Relleno con ceros, ancho mínimo de 8
+    ft_printf("5. |%-8u|\n", unum);     // Alineación a la izquierda, ancho mínimo de 8
+    
+    printf("\n------------------------------------\n");
+    // 3. Flags con %x (hexadecimal en minúscula)
+    printf("6. |%#08x|\n", num);     // Prefijo 0x, relleno con ceros, ancho mínimo de 8
+    printf("7. |%-#8x|\n", num);     // Prefijo 0x, alineación a la izquierda, ancho mínimo de 8
+    ft_printf("6. |%#08x|\n", num);     // Prefijo 0x, relleno con ceros, ancho mínimo de 8
+    ft_printf("7. |%-#8x|\n", num);
+    
+    printf("\n------------------------------------\n");
+    // 5. Flags con %s (cadena de caracteres)
+    printf("11. |%-10s|\n", str);    // Alineación a la izquierda, ancho mínimo de 10
+    printf("12. |%.3s|\n", str);     // Precisión de 3 caracteres, limita la cadena
+    ft_printf("11. |%-10s|\n", str);    // Alineación a la izquierda, ancho mínimo de 10
+    ft_printf("12. |%.3s|\n", str);
+    
+    printf("\n------------------------------------\n");
+    // 6. Flags con %p (puntero)
+    printf("13. |%20p|\n", ptr);     // Ancho mínimo de 20
+    printf("14. |%-20p|\n", ptr);    // Alineación a la izquierda, ancho mínimo de 20
+    ft_printf("13. |%20p|\n", ptr);     // Ancho mínimo de 20
+    ft_printf("14. |%-20p|\n", ptr); */
+
+/*
+    int a = 42, b = -56;
+    unsigned int c = 12345, d = 67890;
+    float e = 3.14159, f = -2.71828;
+    char *str1 = "Hola", *str2 = "Mundo";
+    void *ptr1 = &a, *ptr2 = &b;
+
+    // 1. Múltiples enteros con diferentes flags
+    printf("1. |%+8d| |%-8d| |%08d|\n", a, b, a); 
+    ft_printf("1. |%+8d| |%-8d| |%08d|\n", a, b, a);
+    printf("\n------------------------------------\n");
+    
+    // 2. Enteros con signo y sin signo combinados
+    printf("2. |%#8x| |%#8X| |%08u| |%-8u|\n", a, b, c, d);
+    ft_printf("2. |%#8x| |%#8X| |%08u| |%-8u|\n", a, b, c, d);
+    printf("\n------------------------------------\n");
+    
+    // 3. Números flotantes con ancho mínimo y precisión, mezclados con enteros
+    printf("3. |%8d| |%08d|\n", a, b); 
+    ft_printf("3. |%8d| |%08d|\n", a, b); 
+    printf("\n------------------------------------\n");
+    
+    // 4. Cadenas de caracteres combinadas con precisión y ancho mínimo
+    printf("4. |%-10s| |%.3s| |%10s| |%-8.5s|\n", str1, str2, str1, str2); 
+    ft_printf("4. |%-10s| |%.3s| |%10s| |%-8.5s|\n", str1, str2, str1, str2);
+    printf("\n------------------------------------\n"); 
+    
+    // 5. Punteros combinados con diferentes anchos mínimos
+    printf("5. |%20p| |%-20p|\n", ptr1, ptr2); 
+    ft_printf("5. |%20p| |%-20p|\n", ptr1, ptr2); 
+    printf("\n------------------------------------\n");
+    
+    // 6. Combinación de enteros, flotantes y hexadecimales en una sola línea
+    printf("6. |%+10d| |%-#8x| |%-#8.3X|\n", b, a, d);
+    ft_printf("6. |%+10d| |%-#8x| |%-#8.3X|\n", b, a, d);*/
+
+    int a = 2147483647, b = -2147483648;
+    unsigned int c = 0, d = 4294967295u;
+    char *str1 = "EdgeCaseTesting", *str2 = "OvercomplicatedOutput";
+    void *ptr1 = &a, *ptr2 = NULL;
+    int num;
+
+    // Ejemplo 1: Máximos y mínimos de enteros con precisión y alineación extrema
+    num = printf("1. |%+30.20d| |%-30.20d|\n", a, b);
+    printf("LEN: %d\n", num);
+    num = ft_printf("1. |%+30.20d| |%-30.20d|\n", a, b);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 2: Hexadecimales máximos y mínimos con relleno de ceros y prefijo
+    num = printf("2. |%0#40.30x| |%-#40.30x|\n", a, b);
+    printf("LEN: %d\n", num);
+    num = ft_printf("2. |%0#40.30x| |%-#40.30x|\n", a, b);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 3: Sin signo, cadena truncada y puntero nulo con anchos variados
+    num = printf("3. |%030u| |%.5s| |%-20p|\n", d, str1, ptr2);
+    printf("LEN: %d\n", num);
+    num = ft_printf("3. |%030u| |%.5s| |%-20p|\n", d, str1, ptr2);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 4: Enteros en hexadecimal con precisión elevada y cadenas con alineación mixta
+    num = printf("4. |%#50.45x| |%-20s| |%.10s|\n", d, str2, str1);
+    printf("LEN: %d\n", num);
+    num = ft_printf("4. |%#50.45x| |%-20s| |%.10s|\n", d, str2, str1);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 5: Cadena con ancho extremo, puntero y hexadecimal con mínima precisión y prefijo
+    num = printf("5. |%-60.15s| |%25.5p| |%#-30.4x|\n", str2, ptr1, c);
+    printf("LEN: %d\n", num);
+    num = ft_printf("5. |%-60.15s| |%25.5p| |%#-30.4x|\n", str2, ptr1, c);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 6: Hexadecimal sin signo con precisión, entero negativo con ancho y precisión extremos
+    num = printf("6. |%-#50.45x| |%+60.55d|\n", d, b);
+    printf("LEN: %d\n", num);
+    num = ft_printf("6. |%-#50.45x| |%+60.55d|\n", d, b);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 7: Sin signo con relleno de ceros, cadena truncada y puntero con precisión
+    num = printf("7. |%040u| |%.8s| |%20.10p|\n", d, str1, ptr2);
+    printf("LEN: %d\n", num);
+    num = ft_printf("7. |%040u| |%.8s| |%20.10p|\n", d, str1, ptr2);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 8: Hexadecimal, puntero y sin signo con prefijo y precisión elevada
+    num = printf("8. |%-#60.55x| |%30.15p| |%+050.40u|\n", a, ptr1, d);
+    printf("LEN: %d\n", num);
+    num = ft_printf("8. |%-#60.55x| |%30.15p| |%+050.40u|\n", a, ptr1, d);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 9: Enteros y hexadecimales con relleno de ceros, ancho y precisión altos
+    num = printf("9. |%020.18d| |%#020.18x| |%-#020.18x|\n", a, b, d);
+    printf("LEN: %d\n", num);
+    num = ft_printf("9. |%020.18d| |%#020.18x| |%-#020.18x|\n", a, b, d);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
+
+    // Ejemplo 10: Cadena truncada, entero con signo y hexadecimal con ancho amplio
+    num = printf("10. |%.4s| |%+80.70d| |%-#80.75x|\n", str2, b, a);
+    printf("LEN: %d\n", num);
+    num = ft_printf("10. |%.4s| |%+80.70d| |%-#80.75x|\n", str2, b, a);
+    printf("LEN: %d\n", num);
+    printf("\n------------------------------------\n");
 
     return (0);
 }
