c329aed09e8de7516397c141173ad44c60be4ef4
mfranck340 <franck.yaguargos@edu.uah.es>
2024-10-24 00:10:01 +0200

    norme int

--- files/src_bonus/ft_printf_utils4_bonus.c
+++ files/src_bonus/ft_printf_utils4_bonus.c
@@ -29,156 +29,197 @@ int	ft_numpointer(void *p)
 	return(n_aux);
 }
 
+void	handle_zero_flag(t_flags flags, int number, int *n_str, char *str)
+{
+	int	n_aux;
+
+	if (number < 0)
+		print_char('-');
+	else if (number >= 0 && flags.plus)
+	{
+		print_char('+');
+		(*n_str)++;
+	}
+	else if (flags.space)
+	{
+		print_char(' ');
+		(*n_str)++;
+	}
+	n_aux = *n_str;
+	while (flags.width-- > n_aux)
+		(*n_str) += print_char('0');
+	if (number < 0)
+		print_string(str + 1);
+	else
+		print_string(str);
+}
+
+int is_null_number(int number, t_flags flags)
+{
+	if (number == 0 && flags.dot && flags.precision == 0)
+		return (1);
+	return (0);
+}
+
+int	handle_sign(int number, t_flags flags, int *n_str)
+{
+	if (number < 0)
+    {
+        print_char('-');
+        return (-1);
+    }
+    else if (flags.plus)
+    {
+        print_char('+');
+        (*n_str)++;
+        return (1);
+    }
+    else if (flags.space)
+    {
+        print_char(' ');
+        (*n_str)++;
+        return (1);
+    }
+	return (0);
+}
+
+void	handle_minus_flag(t_flags flags, int number, int *n_str, char *str)
+{
+    int n_aux;
+	int	is_null;
+
+	is_null = is_null_number(number, flags);
+    n_aux = *n_str + handle_sign(number, flags, n_str);
+    if (flags.precision > n_aux)
+    {
+        while (flags.precision-- > n_aux)
+            (*n_str) += print_char('0');
+    }
+    if (is_null)
+    {
+        if (flags.width > 0)
+            (*n_str) += print_char(' ');
+    }
+    else if (number < 0)
+        print_string(str + 1);
+    else
+        print_string(str);
+    n_aux = *n_str;
+    while (flags.width-- > n_aux)
+        (*n_str) += print_char(' ');
+}
+
+void	handle_sign_other(t_flags flags, int number, int *n_str, int n_aux)
+{
+	if (number < 0)
+	{
+		print_char('-');
+		if (flags.precision > n_aux - 1)
+			while (flags.precision-- > n_aux - 1)
+				(*n_str) += print_char('0');  
+	}
+	else
+	{
+		if (flags.plus)
+		{
+			print_char('+');
+			(*n_str)++;
+		}
+		else if (flags.space)
+		{
+			print_char(' ');
+			(*n_str)++;
+		}
+		if (flags.precision > n_aux)
+			while (flags.precision-- > n_aux)
+				(*n_str) += print_char('0');
+	}
+}
+
+void	handle_padding(t_flags flags, int number, int n_aux, int *n_str)
+{
+	if (flags.dot && flags.precision > n_aux)
+	{
+		if (flags.width > flags.precision + 1)
+			while (flags.width-- > flags.precision + 1)
+				n_str += print_char(' ');
+	}
+	else if (number < 0 && flags.precision < n_aux - 1)
+	{
+		if (flags.width > n_aux)
+			while (flags.width > n_aux)
+			{
+				n_str += print_char(' ');
+				flags.width--;
+			}
+	}
+	else
+	{
+		if (flags.width > n_aux + 1)
+			while (flags.width-- > n_aux + 1)
+				n_str += print_char(' ');
+	}
+}
+
+void	handle_padding2(t_flags flags, int n_aux, int *n_str)
+{
+	if (flags.dot && flags.precision > n_aux)
+	{
+		if (flags.width > flags.precision)
+			while (flags.width-- > flags.precision)
+				n_str += print_char(' ');
+	}
+	else
+	{
+		if (flags.width > n_aux)
+			while (flags.width > n_aux)
+			{
+				n_str += print_char(' ');
+				flags.width--;
+			}
+	}
+}
+
+void	handle_other_flag(t_flags flags, int number, int *n_str, char *str)
+{
+	int n_aux;
+	int is_null;
+
+	is_null = is_null_number(number, flags);
+	n_aux = *n_str;
+	if (number < 0 || flags.plus || flags.space)
+		handle_padding(flags, number, n_aux, n_str);
+	else
+		handle_padding2(flags, n_aux, n_str);
+	handle_sign_other(flags, number, n_str, n_aux);
+	if (is_null)
+	{
+		if (flags.width > 0)
+			print_char(' ');
+	}
+	else if (number < 0)
+		print_string(str + 1);
+	else
+		print_string(str);
+}
+
 int	print_int_with_flags(va_list args, t_flags flags)
 {
 	char	*str;
 	int		n_aux;
 	int		number;
 	int		n_str;
-	int		is_null;
 
 	number = va_arg(args, int);
 	str = ft_itoa(number);
 	n_str = ft_strlen(str);
 	n_aux = n_str;
-	is_null = 0;
-	if (number == 0 && flags.dot && flags.precision == 0)
-		is_null = 1;
 	if (flags.zero && flags.dot == 0 && flags.minus == 0)
-	{
-		if (number < 0)
-			print_char('-');
-		else if (number >= 0 && flags.plus)
-		{
-			print_char('+');
-			n_str++;
-			n_aux++;
-		}
-		else if (flags.space)
-		{
-			print_char(' ');
-			n_str++;
-			n_aux++;
-		}
-		while (flags.width-- > n_aux)
-			n_str += print_char('0');
-		if (number < 0)
-			print_string(str + 1);
-		else
-			print_string(str);
-	}
+		handle_zero_flag(flags, number, &n_str, str);
 	else if (flags.minus)
-	{
-		if (number < 0)
-		{
-			print_char('-');
-			n_aux--;
-		}
-		else if (number >= 0 && flags.plus)
-		{
-			print_char('+');
-			n_str++;
-			n_aux++;
-		}
-		else if (flags.space)
-		{
-			print_char(' ');
-			n_str++;
-			n_aux++;
-		}
-		if (flags.precision > n_aux)
-		{
-			while (flags.precision-- > n_aux)
-				n_str += print_char('0');
-		}
-		if (is_null)
-		{
-			if (flags.width > 0)
-				print_char(' ');
-		}
-		else if (number < 0)
-			print_string(str + 1);
-		else
-			print_string(str);
-		n_aux = n_str;
-		while (flags.width-- > n_aux)
-			n_str += print_char(' ');
-	}
+		handle_minus_flag(flags, number, &n_str, str);
 	else
-	{
-		if (number < 0 || flags.plus || flags.space)
-		{
-			if (flags.dot && flags.precision > n_aux)
-			{
-				if (flags.width > flags.precision + 1)
-					while (flags.width-- > flags.precision + 1)
-						n_str += print_char(' ');
-			}
-			else if (number < 0 && flags.precision < n_aux - 1)
-			{
-				if (flags.width > n_aux)
-					while (flags.width > n_aux)
-					{
-						n_str += print_char(' ');
-						flags.width--;
-					}
-			}
-			else
-			{
-				if (flags.width > n_aux + 1)
-					while (flags.width-- > n_aux + 1)
-						n_str += print_char(' ');
-			}
-		}
-		else
-			if (flags.dot && flags.precision > n_aux)
-			{
-				if (flags.width > flags.precision)
-					while (flags.width-- > flags.precision)
-						n_str += print_char(' ');
-			}
-			else
-			{
-				if (flags.width > n_aux)
-					while (flags.width > n_aux)
-					{
-						n_str += print_char(' ');
-						flags.width--;
-					}
-			}
-		if (number < 0)
-		{
-			print_char('-');
-			if (flags.precision > n_aux - 1)
-				while (flags.precision-- > n_aux - 1)
-					n_str += print_char('0');
-		}
-		else
-		{
-			if (flags.plus)
-			{
-				print_char('+');
-				n_str++;
-			}
-			else if (flags.space)
-			{
-				print_char(' ');
-				n_str++;
-			}
-			if (flags.precision > n_aux)
-				while (flags.precision-- > n_aux)
-					n_str += print_char('0');
-		}
-		if (is_null)
-		{
-			if (flags.width > 0)
-				print_char(' ');
-		}
-		else if (number < 0)
-			print_string(str + 1);
-		else
-			print_string(str);
-	}
+		handle_other_flag();
 	free(str);
 	return (n_str);
 }
